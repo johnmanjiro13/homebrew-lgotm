5268fb9702607df49dd40f03ebe80a978c2234b2
goreleaserbot <goreleaser@carlosbecker.com>
2021-08-29 15:30:33 +0900

    Brew formula update for lgotm version v0.4.3

--- lgotm.rb
+++ lgotm.rb
@@ -5,29 +5,29 @@
 class Lgotm < Formula
   desc "lgotm is a command for generation LGTM image and generated image is copied to clipboard."
   homepage "https://github.com/johnmanjiro13/lgotm"
-  version "0.4.2"
+  version "0.4.3"
   license "MIT"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/johnmanjiro13/lgotm/releases/download/v0.4.2/lgotm_Darwin_x86_64.tar.gz"
-      sha256 "3b602a89769329561e2ce7610c1c3dd1f85a3bc3bda29a556db4a0819c56d4c0"
+      url "https://github.com/johnmanjiro13/lgotm/releases/download/v0.4.3/lgotm_Darwin_x86_64.tar.gz"
+      sha256 "60e297df65aac939ccb2de6aa7df29fc1a259a93cd5e2e6f80b512d224be246a"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/johnmanjiro13/lgotm/releases/download/v0.4.2/lgotm_Darwin_arm64.tar.gz"
-      sha256 "62d1b165af0cd10fe130a90623a02133547cc4ad23c9caae4ced092e91d074a3"
+      url "https://github.com/johnmanjiro13/lgotm/releases/download/v0.4.3/lgotm_Darwin_arm64.tar.gz"
+      sha256 "fda28a9897ca947f089ce0785ea67fc255ab9be5ba27c3d3758819b717b7c23d"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/johnmanjiro13/lgotm/releases/download/v0.4.2/lgotm_Linux_x86_64.tar.gz"
-      sha256 "425178bd33fe5de2338c14c48e3e8c87cbad5709294dd06026f0913a021e9fc5"
+      url "https://github.com/johnmanjiro13/lgotm/releases/download/v0.4.3/lgotm_Linux_x86_64.tar.gz"
+      sha256 "667bc3db5c16eb40aecadc7324ab14ffaff6c1907055e6f62a05bac1419e50e2"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/johnmanjiro13/lgotm/releases/download/v0.4.2/lgotm_Linux_arm64.tar.gz"
-      sha256 "3a676f64da55f68e47a8fc519ec9245baf55552be854fe2bc5113f46352e54dd"
+      url "https://github.com/johnmanjiro13/lgotm/releases/download/v0.4.3/lgotm_Linux_arm64.tar.gz"
+      sha256 "9bfecb37a8aa57bbbc1be4a531f23f33157926da093bf7d9f1550fe8a646883e"
     end
   end
 
